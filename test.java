e936c98acd06b84508e4e7dbd219267919a975df
priyulp <93876688+priyulp@users.noreply.github.com>
2025-06-25 19:54:06 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Harness!");
+        System.out.println("Hello, Harness, wellcome everyone!");
     }
 }
