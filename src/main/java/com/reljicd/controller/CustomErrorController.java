2143869189d38b6e958ac99b533b808884e1967f
priyulp <93876688+priyulp@users.noreply.github.com>
2025-06-29 13:39:15 +0530

    Update CustomErrorController.java

--- src/main/java/com/reljicd/controller/CustomErrorController.java
+++ src/main/java/com/reljicd/controller/CustomErrorController.java
@@ -1,42 +1,26 @@
 package com.reljicd.controller;
 
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.boot.web.servlet.error.ErrorController;
 
-public class CustomErrorController implements ErrorController {
-    @RequestMapping("/error")
-    public String handleError() {
-        return "error"; // your error.html page
-    }
-
-    public String getErrorPath() {
-        return "/error";
-    }
-}
-
-
-
-@RestController
+@Controller
 public class CustomErrorController implements ErrorController {
 
     private static final String PATH = "/error";
 
     @RequestMapping(PATH)
-    public ModelAndView error() {
-        return new ModelAndView("/error");
+    public ModelAndView handleError() {
+        return new ModelAndView("error"); // maps to src/main/resources/templates/error.html
     }
 
     @GetMapping("/403")
     public ModelAndView error403() {
-        return new ModelAndView("/403");
+        return new ModelAndView("403"); // maps to src/main/resources/templates/403.html
     }
 
-    @Override
-    public String getErrorPath() {
-        return PATH;
-    }
+    // getErrorPath is deprecated and not needed anymore in Spring Boot 2.3+
+    // So we can safely omit overriding it.
 }
