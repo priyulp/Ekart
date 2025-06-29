c6ff9733998067dbdf7854bbad5c052b8af59a5d
priyulp <93876688+priyulp@users.noreply.github.com>
2025-06-29 13:42:23 +0530

    Update HomeController.java

--- src/main/java/com/reljicd/controller/HomeController.java
+++ src/main/java/com/reljicd/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.reljicd.util.Pager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import java.util.Optional;
 public class HomeController {
 
     private static final int INITIAL_PAGE = 0;
+    private static final int PAGE_SIZE = 5;
 
     private final ProductService productService;
 
@@ -28,19 +30,6 @@ public class HomeController {
     @GetMapping("/home")
     public ModelAndView home(@RequestParam("page") Optional<Integer> page) {
 
-        // Evaluate page. If requested parameter is null or less than 0 (to
-        // prevent exception), return initial size. Otherwise, return value of
-        // param. decreased by 1.
-        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+        // If no page or pag
 
-        Page<Product> products = productService.findAllProductsPageable(new PageRequest(evalPage, 5));
-        Pager pager = new Pager(products);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("products", products);
-        modelAndView.addObject("pager", pager);
-        modelAndView.setViewName("/home");
-        return modelAndView;
-    }
-
-}
+    
